62301ec29ad6240cfc5dd1ec359316ba2296b3d3
CashLee <cashlee96347@gmail.com>
2020-01-20 22:55:16 +0800

    [add]add news list

--- src/Map.js
+++ src/Map.js
@@ -1,6 +1,7 @@
 import React ,  { Component } from 'react'
 import { render } from 'react-dom'
 import { Map, Marker, Popup, Circle, CircleMarker, TileLayer } from 'react-leaflet'
+import ReactList from 'react-list';
 
 const WuhanPosition = [30.58333, 114.26667]
 
@@ -19,29 +20,50 @@ class VirusMap extends Component {
       .then(data => this.setState({ data }));
   }
 
+  componentWillUnmount() {
+
+  }
+
+  renderItem(index, key) {
+    return <div class="item" key={key}>
+      {this.state.data.sources[index].city}  
+      <a href={this.state.data.sources[index].news}>
+        {this.state.data.sources[index].news}
+      </a>
+      </div>;
+  }
+
   render(){
       if(this.state.data){
         return  (
-          <Map noWrap={true}  scrollWheelZoom={true} maxZoom="18" className="map-container" center={WuhanPosition} zoom={5}>
-            <TileLayer
-              noWrap={true} 
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              id="mapbox/streets-v11"
-            />
+          <div>
+            <Map noWrap={true}  scrollWheelZoom={true} maxZoom="18" className="map-container" center={WuhanPosition} zoom={5}>
+              <TileLayer
+                noWrap={true} 
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                id="mapbox/streets-v11"
+              />
 
-            {this.state.data.sources.map( (item, key) => 
-              <Marker key={key} position={item["position"]}>
-                <Popup>{item["city"]} 新型冠状病毒感染个案：{item["number"]} </Popup>
-              </Marker>
-              
-            )}
+              {this.state.data.sources.map( (item, key) => 
+                <Marker key={key} position={item["position"]}>
+                  <Popup>{item["city"]} 新型冠状病毒感染个案：{item["number"]} </Popup>
+                </Marker>
+              )}
 
-            {this.state.data.sources.map( (item, key) => 
-              <CircleMarker key={key} color="red" fillColor="#f03" fillOpacity="0.5" radius="20"  center={item["position"]}>
-              </CircleMarker>
-            )}
-            
-          </Map>
+              {this.state.data.sources.map( (item, key) => 
+                <CircleMarker key={key} color="red" fillColor="#f03" fillOpacity="0.5" radius="20"  center={item["position"]}>
+                </CircleMarker>
+              )}
+            </Map>
+
+            <div class="scroll-list">
+              <ReactList
+                itemRenderer={this.renderItem.bind(this)}
+                length={this.state.data.sources.length}
+                type='uniform'
+              />
+            </div>
+          </div>
         );
       }
       return null;
