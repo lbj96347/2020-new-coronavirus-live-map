a0cff72d070ceb5dc65c6c6ac5e0789c3343fee1
CashLee <cashlee96347@gmail.com>
2020-01-21 20:59:20 +0800

    [add]add nation selector

--- src/Map.js
+++ src/Map.js
@@ -12,11 +12,12 @@ class VirusMap extends Component {
     super(props);
     this.state = {
       data: null,
+      nationIndex: 0
     };
   }
 
   componentDidMount() {
-    const sourceUrl = (ENV === "dis") ? "https://lbj96347.github.io/2020-virus-map/virus-data.json" : "http://localhost:8000/virus-data.json" 
+    const sourceUrl = (ENV === "dis") ? "https://lbj96347.github.io/2020-virus-map/virus-data.json" : "http://localhost:8000/virus-data-nation.json" 
     fetch(sourceUrl)
       .then(response => response.json())
       .then(data => this.setState({ data }));
@@ -26,13 +27,27 @@ class VirusMap extends Component {
 
   }
 
+  handleFocusNation (index){
+    this.setState({
+      nationIndex: index
+    })
+  }
+
   render(){
       if(this.state.data){
         return  (
           <div>
+
             <div className="item">
-              <a>新型冠状病毒地图 - 数据来源央视新闻或者地方卫生部门网站</a>
+              <p>新型冠状病毒地图 - 数据来源央视新闻或者地方卫生部门网站</p>
+              <p>更新时间 Last update：{this.state.data["updateTimestamp"]}</p>
+              <p>国家/地区：
+                {this.state.data.data.map((item, key) => 
+                  <a href="#" key={key} onClick={() => this.handleFocusNation(key)}>{item["nation"]}</a>  
+                )} 
+              </p>
             </div>
+
             <Map noWrap={true}  scrollWheelZoom={true} maxZoom="18" className="map-container" center={WuhanPosition} zoom={5}>
               <TileLayer
                 noWrap={true} 
@@ -41,13 +56,13 @@ class VirusMap extends Component {
                 attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>Mapbox</a>"
               />
 
-              {this.state.data.sources.map( (item, key) => 
+              {this.state.data.data[this.state.nationIndex].sources.map( (item, key) => 
                 <Marker key={key} position={item["position"]}>
                   <Popup> <a href={item["news"]}>{item["city"]}</a> 新型冠状病毒感染个案（含怀疑）：{item["number"]} </Popup>
                 </Marker>
               )}
 
-              {this.state.data.sources.map( (item, key) => 
+              {this.state.data.data[this.state.nationIndex].sources.map( (item, key) => 
                 <CircleMarker key={key} color={ (item["confirmed"] ?  "red" : "yellow") } fillColor={ (item["confirmed"] ? "#f03" : "#ffcf00")} fillOpacity="0.5" radius={ ((item["number"] > 20) ? 20 : item["number"]) }  center={item["position"]}>
                 </CircleMarker>
               )}
