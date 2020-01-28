993dbdd72fcae9bb311828d58d9564197920e720
CashLee <cashlee96347@gmail.com>
2020-01-28 13:35:03 +0800

    [add]add play history feature

--- src/Map.js
+++ src/Map.js
@@ -4,6 +4,9 @@ import { Map, Marker, Popup, CircleMarker, TileLayer } from 'react-leaflet'
 // import ReactList from 'react-list';
 
 const WuhanPosition = [30.58333, 114.26667]
+var historyData = [] // try to render log  
+var renderCount = 0;
+var renderingStat = false;
 const ENV = "dev" // dis   
 
 class VirusMap extends Component {
@@ -14,6 +17,7 @@ class VirusMap extends Component {
       data: null,
       nationIndex: 0
     };
+    this.handleRenderData = this.handleRenderData.bind(this);
   }
 
   componentDidMount() {
@@ -33,6 +37,55 @@ class VirusMap extends Component {
     })
   }
 
+  
+  handleHistoryData(){
+    renderingStat = true
+    if( historyData.length === 0 ){
+      for( var i = 0; i < 14; i++ ){
+        const sourceUrl = (ENV === "dis") ? "https://lbj96347.github.io/2020-virus-map/virus-data-history/virus-data-"+ i +".json" : "http://localhost:8000/virus-data-history/virus-data-"+ i +".json" 
+        fetch(sourceUrl)
+          .then(response => response.json())
+          .then(data => historyData.push(data));
+      }
+      this.handleRenderData()
+    }else{
+      this.handleRenderData()
+    }
+  }
+
+  handleRenderData(){
+    if(renderCount < 14){
+      this.controlRender()
+    }else{
+      this.stopRenderHistory()
+    }
+  }
+
+  controlRender(){
+    var renderData = historyData[renderCount] 
+    this.setState({ data: renderData }, function(){
+      setTimeout(function (){
+        console.log( renderCount );
+        renderCount = renderCount + 1
+        this.handleRenderData()
+      }.bind(this), 2000);
+    })
+  }
+
+  stopRenderHistory(){
+    renderingStat = false
+    renderCount = 0
+  }
+
+  startToRenderHistory(){
+    if( renderingStat === false ){
+      this.handleHistoryData()
+    }else{
+      alert("播放中")
+      return null 
+    }
+  }
+
   render(){
       if(this.state.data){
         return  (
@@ -40,7 +93,7 @@ class VirusMap extends Component {
 
             <div className="item">
               <p>新型冠状病毒地图 - 数据来源央视新闻或者地方卫生部门网站</p>
-              <p>更新时间 Last update：{this.state.data["updateTimestamp"]}</p>
+              <p>Last update：{this.state.data["updateTimestamp"]} <a href="#" onClick={ () => this.startToRenderHistory() }>播放历史</a></p>
               <p>国家/地区：
                 {this.state.data.data.map((item, key) => 
                   <a href="#" key={key} onClick={() => this.handleFocusNation(key)}>{item["nation"]}</a>  
